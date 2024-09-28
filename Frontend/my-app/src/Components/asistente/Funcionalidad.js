b1cb62a5cb21f323f6ff410f9be9c544037bebd5
Christian Blanco <3617836980101@ingenieria.usac.edu.gt>
2024-09-28 17:59:11 -0600

    [ADD] VISTA INICIAL DE ASISTENTE

---
+++ Frontend/my-app/src/Components/asistente/Funcionalidad.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { Button } from 'react-bootstrap';
+import { Settings, SquareUser, SquareArrowDown, Users, Edit } from 'lucide-react';
+
+const iconos = {
+    settings: <Settings className="me-2" />,
+    SquareUser: <SquareUser className="me-2" />,
+    edit: <Edit className="me-2" />,
+    SquareArrowDown: <SquareArrowDown className="me-2" />,
+    Users: <Users className="me-2" />,
+};
+
+const Funcionalidad = ({ icon, texto, link, action, onClick }) => {
+    return (
+        <>
+            <Button variant="outline-dark" className="w-100 mb-3 d-flex align-items-center" href={link} onClick={onClick}>
+                {iconos[icon]}
+                {texto}
+            </Button>
+        </>
+    );
+};
+
+export default Funcionalidad;
--- Frontend/my-app/src/Pages/asistente/asistente.js
+++ Frontend/my-app/src/Pages/asistente/asistente.js
@@ -1,16 +1,44 @@
 import React from "react";
+
 import Navbar from "../../Components/Navbar";
-import Sidebar from "../../Components/SidebarAsistente";
+import Funcionalidad from "../../Components/asistente/Funcionalidad";
+import PerfilUsuario from "../../Components/asistente/PerfilUsuario";
 
 function Asistente() {
+
+    const usuario = {
+        username: "Asistente",
+        email: "asistente@correo.com",
+        url_foto: "https://img.freepik.com/vector-premium/logotipo-chat-bot-asistente-virtual-logotipo-icono-bot-cabeza-robot-auriculares_843540-99.jpg",
+    };
+
     return (
         <>
-            <Navbar rol="Asistente"/>
+            <Navbar rol="Asistente" />
             <div class="container-fluid">
                 <div class="row flex-nowrap">
-                    <Sidebar></Sidebar>
-                    <div class="col py-3 px-3 pt-3">
-                        <h1>¡Bienvenida asistente!</h1>
+                    <div className="col-md-8 container">
+                        <PerfilUsuario usuario={usuario} />
+                        <div className="row mt-4">
+                            <div className="col-md-6">
+                                <Funcionalidad icon="settings" texto="Configuración de la cuenta" link="" />
+                            </div>
+                            <div className="col-md-6">
+                                <Funcionalidad icon="edit" texto="Solicitudes de empleo" link="" />
+                            </div>
+                            <div className="col-md-6">
+                                <Funcionalidad icon="SquareUser" texto="Conductores" link="" />
+                            </div>
+                            <div className="col-md-6">
+                                <Funcionalidad icon="Users" texto="Usuarios" link="" />
+                            </div>
+                            {/* <div className="col-md-6">
+                                <Funcionalidad icon="SquareArrowDown" texto="Baja de conductores" link="" />
+                            </div>
+                            <div className="col-md-6">
+                                <Funcionalidad icon="SquareArrowDown" texto="Baja de usuarios" link="" />
+                            </div> */}
+                        </div>
                     </div>
                 </div>
             </div>
